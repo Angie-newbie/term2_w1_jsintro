790b89b321bcbe7174f8b2f145e3c45cc65924f4
Angie Newbie <angel890817@hotmail.com>
2025-04-02 18:08:10 +0900

    arrays

--- intro.js
+++ intro.js
@@ -2,3 +2,30 @@ a = 1;
 console.log(a);
 
 
+console.log(Boolean('Hi'))
+//Number
+console.log(typeof('Hi'))
+console.log(typeof(2))
+
+console.log(1/2)
+console.log(1/ "2")
+console.log(1/0)
+console.log(1/"hi")
+
+//Explicit cast
+x=2
+console.log(1/Number(x))
+
+//Array (list in python
+const names = ['John', 'Jane', 'Bob']
+const foo = ['Matt, 52, 184.5']
+console.log(names[1])
+names.push('Mary', 'Jason')
+console.log(names)
+const name = names.pop()
+console.log(names)
+console.log(name)
+
+const bigArray = names.concat((foo))
+console.log(bigArray)
+console.log(names)
