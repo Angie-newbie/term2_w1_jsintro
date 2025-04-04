7e6180a8ac8fb54ce9242c304caa7e9bccff557c
Angie Newbie <angel890817@hotmail.com>
2025-04-05 08:21:36 +0900

    vs code testing

---
+++ loops.js
@@ -0,0 +1,23 @@
+//Python
+//count = 4
+//while count > 0 :
+//  print(count)
+//  count -= 1
+
+
+// let count = 4
+// while (count > 0) {
+//     console.log(count)
+//     count --
+// } ;
+
+//Python
+//for i in range(10)
+//  print(i)
+
+// 3 parts for loop
+// for (Initializer; condition; post-iter)
+
+for(let i = 0; i < 10; i++) {
+    console.log(i)
+}
