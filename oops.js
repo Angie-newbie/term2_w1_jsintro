a0777d3de23106252f87ebcf92392bb94dd37bf1
Angie Newbie <angel890817@hotmail.com>
2025-04-05 11:48:26 +0900

    oop

--- oops.js
+++ oops.js
@@ -3,7 +3,7 @@ const person = {
     name : "John",
     age : 34
 }
-console.log(typeof person)
+console.log(typeof person);
 
 //Python class
 // class Person:
@@ -13,18 +13,32 @@ console.log(typeof person)
 
 //  p1 = Person('John', 35)
 
-//Constructor
-function Person(name, age){
-    //copy param  values into object properties
-    this.name = name
-    this.age = age
-    // this.greet = () => {
-    //     console.log(`${name} is ${age} years old`)
-    // }
-}
+// //Constructor
+// function Person(name, age){
+//     //copy param  values into object properties
+//     this.name = name
+//     this.age = age
+//     // this.greet = () => {
+//     //     console.log(`${name} is ${age} years old`)
+//     // }
+// }
 
-Person.prototype.greet = () => {
-    console.log(`${this.name} is ${this.age} years old`)
+// //Attach greet method to the prototype
+// Person.prototype.greet = () => {
+//     console.log(`${this.name} is ${this.age} years old`)
+// }
+
+
+class Person {
+    constructor(name, age){
+    //copy param  values into object properties
+        this.name = name
+        this.age = age
+    }
+
+    greet = () => {
+        console.log(`${this.name} is ${this.age} years old`)
+    }
 }
 
 const p1 = new Person('Matt', 52)
@@ -33,3 +47,47 @@ p1.greet()
 p2.age = 61
 p2.greet()
 console.log(p2)
+
+class Rectangle {
+    constructor(width,height) {
+        this.width = width
+        this.height = height
+
+    }
+
+    get width() {
+        return this._width
+    }
+
+    set width(value){
+        //check auth
+        // validate value
+        this._width = value
+    }
+
+    get area() {
+        //Check autg
+        //if (auth) {}
+        return this.height * this.width
+        // else (raise an exception)
+    }
+}
+
+const r1 = new Rectangle(10,20)
+r1.width = 5
+console.log(r1.area)
+
+// Inneritance
+// Python
+// class Square(Rectangle):
+
+class Square extends Rectangle {
+    constructor(size){
+        super(size, size)
+    }
+
+}
+
+const s1 = new Square(10)
+console.log(s1)
+console.log(s1.area)
